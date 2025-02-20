3eb4d9212e4fb47ad9780b6ed299cbcb4bdc3d28
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-02-20 21:35:07 +0600

    setup eloquent model relations for customer, customer addressess and user model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
+use App\Enums\AddressType;
+use App\Models\CustomerAddress;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id', 'user_id');
+    }
+
+    private function _getAddresses(): HasOne
+    {
+        return $this->hasOne(CustomerAddress::class, 'customer_id', 'user_id');
+    }
+
+    public function shippingAddress(): HasOne
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::Shipping->value);
+    }
+
+    public function billingAddress(): HasOne
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::Billing->value);
+    }
 }
