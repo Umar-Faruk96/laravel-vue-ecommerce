c69289d3fe2ec1ea4e3d4ddac84f229f62d3e926
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-04-28 22:43:48 +0600

    create, update and delete user from admin implemented

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -7,25 +7,26 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateUserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'name' => 'sometimes|required|string|max:55',
-            'email' => 'sometimes|required|email|unique:users,email,' . $this->user->id,
-            'password' => 'sometimes|required|' . Password::min(8)->letters()->numbers()->symbols()            
-        ];
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 */
+	public function authorize() : bool
+	{
+		return true;
+	}
+	
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+	 */
+	public function rules() : array
+	{
+		return [
+			'name' => 'sometimes|required|string|max:55',
+			'email' => 'sometimes|required|email|unique:users,email,' . $this->user->id,
+			'password' => ['sometimes', 'required', 'confirmed', Password::min(8)->letters()->numbers()->symbols()],
+			'password_confirmation' => ['required_with:password', Password::min(8)->letters()->numbers()->symbols()]
+		];
+	}
 }
