a1a8867a1ee713b7769eae19fb38fb56403d6c73
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-05-20 22:46:06 +0600

    create cards, load data, format and style vue component

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -6,34 +6,35 @@ use App\Http\Controllers\Controller;
 use App\Models\{Customer, Product, Order};
 use App\Enums\CustomerStatus;
 use App\Enums\OrderStatus;
+use Illuminate\Http\JsonResponse;
 
 class DashboardController extends Controller
 {
-    public function activeCustomers()
-    {
-        $activeCustomers = Customer::where('status', CustomerStatus::Active->value)->count();
-        return response()->json(['active_customers' => $activeCustomers]);
-    }
-
-    public function activeProducts()
-    {
-        // TODO: Implement the logic to count active products
-        $activeProducts = Product::count();
-
-        return response()->json(['active_products' => $activeProducts]);
-    }
-
-    public function paidOrders()
-    {
-        $paidOrders = Order::where('status', OrderStatus::Paid->value)->count();
-
-        return response()->json(['paid_orders' => $paidOrders]);
-    }
-
-    public function totalSale()
-    {
-        $totalSale = Order::where('status', OrderStatus::Paid->value)->sum('total_price');
-
-        return response()->json(['total_sale' => $totalSale]);
-    }
+	public function activeCustomers() : JsonResponse
+	{
+		$activeCustomers = Customer::where('status', CustomerStatus::Active->value)->count();
+		return response()->json(['active_customers' => $activeCustomers]);
+	}
+	
+	public function activeProducts() : JsonResponse
+	{
+		// TODO: Implement the logic to count active products
+		$activeProducts = Product::count('id');
+		
+		return response()->json(['active_products' => $activeProducts]);
+	}
+	
+	public function paidOrders() : JsonResponse
+	{
+		$paidOrders = Order::where('status', OrderStatus::Paid->value)->count();
+		
+		return response()->json(['paid_orders' => $paidOrders]);
+	}
+	
+	public function totalSale() : JsonResponse
+	{
+		$totalSale = Order::where('status', OrderStatus::Paid->value)->sum('total_price');
+		
+		return response()->json(['total_sale' => $totalSale]);
+	}
 }
--- vue-admin-panel/src/components/core/Spinner.vue
+++ vue-admin-panel/src/components/core/Spinner.vue
@@ -1,14 +1,25 @@
-<script setup></script>
 <template>
-    <section class="bg-gray-200 flex items-center justify-center">
-        <main class="flex flex-col items-center text-gray-700">
-            <svg class="animate-spin -ml-1 h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                </path>
-            </svg>
-            <div class="mt-2">Please Wait...</div>
-        </main>
-    </section>
+  <section class="flex items-center justify-center" :class>
+    <main class="flex flex-col items-center text-gray-700">
+      <svg class="animate-spin -ml-1 h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+        </path>
+      </svg>
+      <div class="mt-2" v-if="text">{{ text }}</div>
+    </main>
+  </section>
 </template>
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: "Please Wait..."
+  },
+  class: {
+    type: String,
+    default: "bg-gray-200"
+  }
+})
+</script>
