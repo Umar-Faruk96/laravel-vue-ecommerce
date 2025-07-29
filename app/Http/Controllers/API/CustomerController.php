b1304cb119c4d19794a77fc2838144a5cf10f1ff
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-07-29 16:36:43 +0600

    add db transaction on api customer controller

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -20,88 +20,102 @@ use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
-	public function index(Request $request) : JsonResource
+	public function index(Request $request): JsonResource
 	{
 		$perPage = $request->get('per_page', 10);
 		$search = $request->get('search', '');
 		$sortBy = $request->get('sort_by', 'updated_at');
 		$sortTo = $request->get('sort_to', 'desc');
-		
+
 		$query = Customer::query()->orderBy($sortBy, $sortTo);
-		
-		if($search) {
+
+		if ($search) {
 			$query->where(DB::raw("CONCAT_WS(' ', first_name, last_name )"), 'like', "%$search%")
 				->orWhere('email', 'like', "%$search%")
 				->orWhere('phone', 'like', "%$search%")
 				->orWhere('status', 'like', "%$search%");
 		}
-		
+
 		return CustomerListResource::collection($query->paginate($perPage));
 	}
-	
-	public function show(Customer $customer) : JsonResource
+
+	public function show(Customer $customer): JsonResource
 	{
 		return CustomerResource::make($customer);
 	}
-	
-	public function store(CreateCustomerRequest $request) : JsonResource
+
+	public function store(CreateCustomerRequest $request): JsonResource
 	{
 		$customerData = $request->validated();
-		
+
 		$customerData['status'] = $customerData['status'] ? CustomerStatus::Active->value : CustomerStatus::Disabled->value;
 		$customerData['created_by'] = request()->user()->id;
 		$customerData['updated_by'] = request()->user()->id;
-		
-		$customer = Customer::create($customerData);
-		
-		$customer->shippingAddress()->create([
-			'customer_id' => $customer->user_id,
-			'type' => AddressType::Shipping,
-			...$customerData['shippingAddress']
-		]);
-		
-		$customer->billingAddress()->create([
-			'customer_id' => $customer->user_id,
-			'type' => AddressType::Billing,
-			...$customerData['billingAddress']
-		]);
-		
+
+		DB::beginTransaction();
+		try {
+			$customer = Customer::create($customerData);
+
+			$customer->shippingAddress()->create([
+				'customer_id' => $customer->user_id,
+				'type' => AddressType::Shipping,
+				...$customerData['shippingAddress']
+			]);
+
+			$customer->billingAddress()->create([
+				'customer_id' => $customer->user_id,
+				'type' => AddressType::Billing,
+				...$customerData['billingAddress']
+			]);
+		} catch (\Exception $exception) {
+			DB::rollBack();
+			throw  new \Exception('Error creating customer: ' . $exception->getMessage());
+		}
+		DB::commit();
+
 		return CustomerResource::make($customer);
 	}
-	
-	public function update(UpdateCustomerRequest $request, Customer $customer) : JsonResource
+
+	public function update(UpdateCustomerRequest $request, Customer $customer): JsonResource
 	{
 		$customerData = $request->validated();
-		
+
 		$customerData['status'] = $customerData['status'] ? CustomerStatus::Active->value : CustomerStatus::Disabled->value;
 		$customerData['updated_by'] = $customerData['id'] ?? request()->user()->id;
-		
-		$customer->update($customerData);
-		
-		/*$customerData['shippingAddress'] = [
-			'customer_id' => $customer->user_id,
-			'type' => AddressType::Shipping->value,
-			...$customerData['shippingAddress']
-		];*/
-		
-		// $customer->shippingAddress()->update($customerData['shippingAddress']);
-		
-		$customer->shippingAddress()->updateOrCreate(['customer_id' => $customer->user_id, 'type' => AddressType::Shipping], $customerData['shippingAddress']);
-		$customer->billingAddress()->updateOrCreate(['customer_id' => $customer->user_id, 'type' => AddressType::Billing], $customerData['billingAddress']);
-		
+
+		DB::beginTransaction();
+		try {
+			$customer->update($customerData);
+
+			/*$customerData['shippingAddress'] = [
+				'customer_id' => $customer->user_id,
+				'type' => AddressType::Shipping->value,
+				...$customerData['shippingAddress']
+			];*/
+
+			// $customer->shippingAddress()->update($customerData['shippingAddress']);
+
+			$customer->shippingAddress()->updateOrCreate(['customer_id' => $customer->user_id, 'type' => AddressType::Shipping], $customerData['shippingAddress']);
+			$customer->billingAddress()->updateOrCreate(['customer_id' => $customer->user_id, 'type' => AddressType::Billing], $customerData['billingAddress']);
+		} catch (\Exception $exception) {
+			DB::rollBack();
+			throw new \Exception('Error updating customer: ' . $exception->getMessage());
+		}
+		DB::commit();
+
 		return CustomerResource::make($customer);
 	}
-	
-	public function destroy(Customer $customer) : JsonResponse
+
+	public function destroy(Customer $customer): JsonResponse
 	{
 		$customer->delete();
-		
+
 		return response()->json([
 			'message' => 'Customer deleted successfully'
 		]);
 	}
-	
-	public function getCountries() : AnonymousResourceCollection
+
+	public function getCountries(): AnonymousResourceCollection
 	{
 		return CountryResource::collection(Country::query()->orderBy('name', 'asc')->get());
 	}
