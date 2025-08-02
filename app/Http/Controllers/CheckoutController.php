3657fb29ffb63b1d7abdf31b8e513e4330ffaf94
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-03 01:01:49 +0600

    fix route name on checkout controller

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -22,7 +22,7 @@ class CheckoutController extends Controller
         $customer = $user->customer;
 
         if (!$customer->billingAddress || !$customer->shippingAddress) {
-            return redirect()->route('profile')->withErrors(['error' => 'Please provide your address details first']);
+            return redirect()->route('profile.show')->withErrors(['error' => 'Please provide your address details first']);
         }
 
         $stripe = new StripeClient(config('stripe.stripe_sk'));
