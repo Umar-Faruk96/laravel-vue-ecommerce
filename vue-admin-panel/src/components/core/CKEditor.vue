4d8ceb36edb9c076405336bbfb276578255f88db
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-07 18:02:00 +0600

    admin: remove premium plugin from CKEditor component, fix bug

--- vue-admin-panel/src/components/core/CKEditor.vue
+++ vue-admin-panel/src/components/core/CKEditor.vue
@@ -31,11 +31,10 @@ const config = computed(() => {
   }
 
   const {Essentials, Paragraph, Bold, Italic} = cloud.data.value.CKEditor;
-  const {FormatPainter} = cloud.data.value.CKEditorPremiumFeatures;
 
   return {
     licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3ODYxNDcxOTksImp0aSI6ImRhNjZkNWM1LTEwMzUtNDVjZi05OWIzLWI4NTgxOWUxZWY1NCIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIiwiRTJQIiwiRTJXIl0sInZjIjoiM2IwZTA2YjQifQ.s7BLsta_ZMcpBQl6AFfkb1oeCOJV5rn72D7rgnk3CX-5Iu3GhbXe4HkSJBjXGQ1MP7xUl-XdY0biw2jfyADs-A',
-    plugins: [Essentials, Paragraph, Bold, Italic, FormatPainter],
+    plugins: [Essentials, Paragraph, Bold, Italic],
     toolbar: ['undo', 'redo', '|', 'bold', 'italic', '|', 'formatPainter']
   };
 });
