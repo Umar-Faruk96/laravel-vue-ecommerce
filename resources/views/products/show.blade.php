9c03e92e55500ddebdb65e3c4d7dadf2bb3dfc49
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-07 22:01:59 +0600

    add html support on product desc page
    revert tailwind on wysiwyg container

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -31,23 +31,23 @@
                     <div class="relative dark:bg-gray-800">
                         <template x-for="image in images">
                             <div x-show="activeImage === image" class="aspect-h-2 aspect-w-3">
-                                <img :src="image" alt="" class="mx-auto w-auto" />
+                                <img :src="image" alt="" class="mx-auto w-auto"/>
                             </div>
                         </template>
 
                         <a @click.prevent="prev"
-                            class="absolute left-0 top-1/2 ml-2 -translate-y-1/2 cursor-pointer rounded-sm bg-black/30 text-white dark:bg-gray-500">
+                           class="absolute left-0 top-1/2 ml-2 -translate-y-1/2 cursor-pointer rounded-sm bg-black/30 text-white dark:bg-gray-500">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                                 stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7"/>
                             </svg>
                         </a>
 
                         <a @click.prevent="next"
-                            class="absolute right-0 top-1/2 mr-2 -translate-y-1/2 cursor-pointer rounded-sm bg-black/30 text-white dark:bg-gray-500">
+                           class="absolute right-0 top-1/2 mr-2 -translate-y-1/2 cursor-pointer rounded-sm bg-black/30 text-white dark:bg-gray-500">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                                 stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"/>
                             </svg>
                         </a>
                     </div>
@@ -55,9 +55,9 @@
                     <div class="flex">
                         <template x-for="image in images">
                             <a @click.prevent="activeImage = image"
-                                class="flex h-[80px] w-[80px] cursor-pointer items-center justify-center border border-gray-300 hover:border-purple-500 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-purple-500"
-                                :class="{ 'border-purple-600 dark:border-purple-600': activeImage === image }">
-                                <img :src="image" alt="" class="max-auto max-h-full w-auto" />
+                               class="flex h-[80px] w-[80px] cursor-pointer items-center justify-center border border-gray-300 hover:border-purple-500 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-purple-500"
+                               :class="{ 'border-purple-600 dark:border-purple-600': activeImage === image }">
+                                <img :src="image" alt="" class="max-auto max-h-full w-auto"/>
                             </a>
                         </template>
                     </div>
@@ -77,27 +77,27 @@
                     </label>
 
                     <input type="number" name="quantity" x-ref="quantityEl" value="1" min="1"
-                        class="w-32 rounded focus:border-purple-500 focus:outline-none dark:border-2 dark:focus:border-purple-600" />
+                           class="w-32 rounded focus:border-purple-500 focus:outline-none dark:border-2 dark:focus:border-purple-600"/>
                 </div>
 
                 <button @click="addToCart($refs.quantityEl.value)"
-                    class="btn-primary flex w-full min-w-0 justify-center py-4 text-lg">
+                        class="btn-primary flex w-full min-w-0 justify-center py-4 text-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="2">
+                         stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
                     </svg>
                     Add to Cart
                 </button>
 
                 <div x-data="{ expanded: false }">
                     <div x-show="expanded" x-collapse.min.120px class="wysiwyg-content text-gray-300">
-                        {{ $product->description }}
+                        {!!  $product->description !!}
                     </div>
 
                     <p class="text-right">
                         <button @click="expanded = !expanded" class="text-purple-500 hover:text-purple-700"
-                            x-text="expanded ? 'Read Less' : 'Read More'"></button>
+                                x-text="expanded ? 'Read Less' : 'Read More'"></button>
                     </p>
                 </div>
             </div>
