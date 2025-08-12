3d23a35ca190062df50eafb4e741195f9d82acc2
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-13 03:32:10 +0600

    admin: make sensitive info like api secret keys hidden

--- vue-admin-panel/src/components/core/CKEditor.vue
+++ vue-admin-panel/src/components/core/CKEditor.vue
@@ -1,10 +1,10 @@
 <template>
-  <ckeditor
-      v-if="editor"
-      v-model="data"
-      :editor="editor"
-      :config="config" tag-name="textarea"
-  />
+    <ckeditor
+        v-if="editor"
+        v-model="data"
+        :editor="editor"
+        :config="config" tag-name="textarea"
+    />
 </template>
 
 <script setup>
@@ -12,87 +12,86 @@ import {ref, computed} from 'vue';
 import {Ckeditor, useCKEditorCloud} from '@ckeditor/ckeditor5-vue';
 
 const cloud = useCKEditorCloud({
-  version: '46.0.0',
+    version: '46.0.0',
 });
 
 const data = ref('<p>Hello world!</p>');
 
 
 const editor = computed(() => {
-  if (!cloud.data.value) {
-    return null;
-  }
+    if (!cloud.data.value) {
+        return null;
+    }
 
-  return cloud.data.value.CKEditor.ClassicEditor;
+    return cloud.data.value.CKEditor.ClassicEditor;
 });
 
 const config = computed(() => {
-      if (!cloud.data.value) {
-        return null;
-      }
+        if (!cloud.data.value) {
+            return null;
+        }
 
-      const {
-        Essentials,
-        Autoformat,
-        TextTransformation,
-        Paragraph,
-        Heading,
-        Alignment,
-        Bold,
-        Italic,
-        Underline,
-        Strikethrough,
-        Subscript,
-        Superscript,
-        BlockQuote,
-        Code,
-        Link, AutoLink,
-        Image,
-        ImageToolbar, ImageInsert,
-        ImageCaption,
-        ImageResize,
-        ImageStyle,
-        LinkImage,
-        MediaEmbed,
-        Table,
-        TableToolbar,
-        List, TodoList,
-        Indent,
-        IndentBlock, RemoveFormat
-      } = cloud.data.value.CKEditor;
+        const {
+            Essentials,
+            Autoformat,
+            TextTransformation,
+            Paragraph,
+            Heading,
+            Alignment,
+            Bold,
+            Italic,
+            Underline,
+            Strikethrough,
+            Subscript,
+            Superscript,
+            BlockQuote,
+            Code,
+            Link, AutoLink,
+            Image,
+            ImageToolbar, ImageInsert,
+            ImageCaption,
+            ImageResize,
+            ImageStyle,
+            LinkImage,
+            MediaEmbed,
+            Table,
+            TableToolbar,
+            List, TodoList,
+            Indent,
+            IndentBlock, RemoveFormat
+        } = cloud.data.value.CKEditor;
 
-      return {
-        // development license key
-        // licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3ODYxNDcxOTksImp0aSI6ImRhNjZkNWM1LTEwMzUtNDVjZi05OWIzLWI4NTgxOWUxZWY1NCIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIiwiRTJQIiwiRTJXIl0sInZjIjoiM2IwZTA2YjQifQ.s7BLsta_ZMcpBQl6AFfkb1oeCOJV5rn72D7rgnk3CX-5Iu3GhbXe4HkSJBjXGQ1MP7xUl-XdY0biw2jfyADs-A',
-        licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3ODYxNDcxOTksImp0aSI6IjdmZDhiMzY1LTQyOTctNDk3Ni04ZDY5LTlkNjQ0MmY2ODQ4NiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCIsIkUyUCIsIkUyVyJdLCJ2YyI6ImYzNjZkOGY5In0.gABitp5nNJmtIswjRSZ6LgqyNUauceTTJHDzVLKKfECSfFtwueWrLwrgSJlsHmHx0GFK8SBLn8hXTHLAF_67XA',
-        plugins: [Essentials, Autoformat, TextTransformation, Paragraph,
-          Heading, Alignment, Bold, Italic,
-          Underline, Strikethrough, Subscript, Superscript, BlockQuote, Code, Link, AutoLink,
-          Image, ImageInsert, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage,
-          MediaEmbed, Table, TableToolbar, List, TodoList, Indent,
-          IndentBlock, RemoveFormat],
-        toolbar: ['undo', 'redo', '|', 'heading', '|', 'alignment', '|', 'bold',
-          'italic',
-          'underline', 'strikethrough', 'subscript', 'superscript', 'blockQuote', 'code', '|',
-          'link', 'insertImage', 'mediaEmbed', '|', 'insertTable', '|', 'bulletedList',
-          'numberedList', 'todoList', 'indent',
-          'outdent', '|', 'removeFormat'],
-        image: {
-          toolbar: [
-            'imageStyle:block',
-            'imageStyle:side',
-            '|',
-            'toggleImageCaption',
-            'imageTextAlternative',
-            '|',
-            'linkImage'
-          ]
-        },
-        table: {
-          contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
-          defaultHeadings: {rows: 1, columns: 1}
-        },
-      };
+        return {
+            // Use production license key for both production and local environments
+            licenseKey: (import.meta.env.VITE_APP_ENV === 'local' || !import.meta.env.DEV) ? import.meta.env.VITE_CKEDITOR_PROD_LICENSE : import.meta.env.VITE_CKEDITOR_DEV_LICENSE,
+            plugins: [Essentials, Autoformat, TextTransformation, Paragraph,
+                Heading, Alignment, Bold, Italic,
+                Underline, Strikethrough, Subscript, Superscript, BlockQuote, Code, Link, AutoLink,
+                Image, ImageInsert, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage,
+                MediaEmbed, Table, TableToolbar, List, TodoList, Indent,
+                IndentBlock, RemoveFormat],
+            toolbar: ['undo', 'redo', '|', 'heading', '|', 'alignment', '|', 'bold',
+                'italic',
+                'underline', 'strikethrough', 'subscript', 'superscript', 'blockQuote', 'code', '|',
+                'link', 'insertImage', 'mediaEmbed', '|', 'insertTable', '|', 'bulletedList',
+                'numberedList', 'todoList', 'indent',
+                'outdent', '|', 'removeFormat'],
+            image: {
+                toolbar: [
+                    'imageStyle:block',
+                    'imageStyle:side',
+                    '|',
+                    'toggleImageCaption',
+                    'imageTextAlternative',
+                    '|',
+                    'linkImage'
+                ]
+            },
+            table: {
+                contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+                defaultHeadings: {rows: 1, columns: 1}
+            },
+        };
     }
 );
 </script>
--- vue-admin-panel/src/views/Orders/OrderDetails.vue
+++ vue-admin-panel/src/views/Orders/OrderDetails.vue
@@ -1,162 +1,162 @@
 <template>
-  <Spinner v-if="loading" class="mt-4" />
-  <template v-else>
-    <div v-if="order" class="bg-black/10 p-4 rounded-lg shadow animate-fade-in-down">
-      <div class="flex items-center justify-between mb-3">
-        <h1 class="text-black/60 dark:text-white/70 text-3xl font-semibold">Order Summary</h1>
+    <Spinner v-if="loading" class="mt-4"/>
+    <template v-else>
+        <div v-if="order" class="bg-black/10 p-4 rounded-lg shadow animate-fade-in-down">
+            <div class="flex items-center justify-between mb-3">
+                <h1 class="text-black/60 dark:text-white/70 text-3xl font-semibold">Order Summary</h1>
 
-        <OrderStatus :order="order" />
+                <OrderStatus :order="order"/>
 
-        <!-- <pre>{{ order }}</pre> -->
-      </div>
-
-      <div class="text-black/60 dark:text-white/60 bg-black/10 p-4 rounded-lg shadow">
-        <!-- Order Details -->
-        <div class="flex justify-between">
-          <!--  Order Info-->
-          <div>
-            <h2
-                class="flex justify-between items-center text-xl font-semibold pb-2 border-b border-gray-600"
-            >
-              Order Info
-            </h2>
-            <table>
-              <tbody>
-              <tr>
-                <td class="font-bold py-1 px-2">Order #</td>
-                <td>{{ order.id }}</td>
-              </tr>
-              <tr>
-                <td class="font-bold py-1 px-2">Order Date</td>
-                <td>{{ order.created_at }}</td>
-              </tr>
-              <tr>
-                <td class="font-bold py-1 px-2">Order Status</td>
-                <td>
-                  <select
-                      @change="changeStatus"
-                      v-model="order.status"
-                      name="statuses"
-                      id="statuses"
-                      class="text-white/90 bg-black/70 px-1 py-0.5 rounded-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-blue-500 capitalize"
-                  >
-                    <option v-for="status in orderStatuses" :value="status">
-                      {{ status }}
-                    </option>
-                  </select>
-                </td>
-              </tr>
-              <tr>
-                <td class="font-bold py-1 px-2">SubTotal</td>
-                <td>${{ order.total_price }}</td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-          <!--/  Order Info-->
-          <!--  Customer Details-->
-          <div>
-            <h2 class="text-xl font-semibold pb-2 border-b border-gray-600">
-              Customer Details
-            </h2>
-            <table>
-              <tbody>
-              <tr>
-                <td class="font-bold py-1 px-2">Full Name</td>
-                <td>{{ order.customer.first_name }} {{ order.customer.last_name }}</td>
-              </tr>
-              <tr>
-                <td class="font-bold py-1 px-2">Email</td>
-                <td>{{ order.customer.email }}</td>
-              </tr>
-              <tr>
-                <td class="font-bold py-1 px-2">Phone</td>
-                <td>{{ order.customer.phone }}</td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-          <!--/  Customer Details-->
-          <!--  Addresses Details-->
-          <div class="flex flex-col gap-3">
-            <div>
-              <h2 class="text-xl font-semibold pb-2 border-b border-gray-600">
-                Billing Address
-              </h2>
-              <!--  Billing Address Details-->
-              <div>
-                {{ order.customer.billing_address.house_number }},
-                {{ order.customer.billing_address.area }} <br />
-                {{ order.customer.billing_address.city }},
-                {{ order.customer.billing_address.zipcode }} <br />
-                {{ order.customer.billing_address.state }},
-                {{ order.customer.billing_address.country.name }} <br />
-              </div>
-              <!--/  Billing Address Details-->
+                <!-- <pre>{{ order }}</pre> -->
             </div>
-            <div>
-              <h2 class="text-xl font-semibold pb-2 border-b border-gray-600">
-                Shipping Address
-              </h2>
-              <!--  Shipping Address Details-->
-              <div>
-                {{ order.customer.shipping_address.house_number }},
-                {{ order.customer.shipping_address.area }} <br />
-                {{ order.customer.shipping_address.city }},
-                {{ order.customer.shipping_address.zipcode }} <br />
-                {{ order.customer.shipping_address.state }},
-                {{ order.customer.shipping_address.country.name }} <br />
-              </div>
-              <!--/  Shipping Address Details-->
-            </div>
-          </div>
-          <!--/  Customer Details-->
-        </div>
-        <!--/ Order Details -->
 
-        <!--    Order Items-->
-        <div>
-          <h2 class="text-xl font-semibold mt-6 pb-2 border-b border-gray-600">
-            Order Items
-          </h2>
-          <div v-for="item of order.items">
-            <!-- Order Item -->
-            <div class="flex flex-col sm:flex-row items-center gap-4 mt-6">
-              <a
-                  href="#"
-                  class="w-36 h-32 flex items-center justify-center overflow-hidden"
-              >
-                <img :src="item.product.image" class="object-cover" alt="" />
-              </a>
-              <div class="flex flex-col justify-between flex-1">
-                <div class="flex justify-between mb-3">
-                  <h3>
-                    {{ item.product.title }}
-                  </h3>
+            <div class="text-black/60 dark:text-white/60 bg-black/10 p-4 rounded-lg shadow">
+                <!-- Order Details -->
+                <div class="flex justify-between">
+                    <!--  Order Info-->
+                    <div>
+                        <h2
+                            class="flex justify-between items-center text-xl font-semibold pb-2 border-b border-gray-600"
+                        >
+                            Order Info
+                        </h2>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td class="font-bold py-1 px-2">Order #</td>
+                                <td>{{ order.id }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold py-1 px-2">Order Date</td>
+                                <td>{{ order.created_at }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold py-1 px-2">Order Status</td>
+                                <td>
+                                    <select
+                                        @change="changeStatus"
+                                        v-model="order.status"
+                                        name="statuses"
+                                        id="statuses"
+                                        class="text-white/90 bg-black/70 px-1 py-0.5 rounded-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-blue-500 capitalize"
+                                    >
+                                        <option v-for="status in orderStatuses" :value="status">
+                                            {{ status }}
+                                        </option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold py-1 px-2">SubTotal</td>
+                                <td>${{ order.total_price }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!--/  Order Info-->
+                    <!--  Customer Details-->
+                    <div>
+                        <h2 class="text-xl font-semibold pb-2 border-b border-gray-600">
+                            Customer Details
+                        </h2>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td class="font-bold py-1 px-2">Full Name</td>
+                                <td>{{ order.customer.first_name }} {{ order.customer.last_name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold py-1 px-2">Email</td>
+                                <td>{{ order.customer.email }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-bold py-1 px-2">Phone</td>
+                                <td>{{ order.customer.phone }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!--/  Customer Details-->
+                    <!--  Addresses Details-->
+                    <div class="flex flex-col gap-3">
+                        <div>
+                            <h2 class="text-xl font-semibold pb-2 border-b border-gray-600">
+                                Billing Address
+                            </h2>
+                            <!--  Billing Address Details-->
+                            <div>
+                                {{ order.customer.billing_address.house_number }},
+                                {{ order.customer.billing_address.area }} <br/>
+                                {{ order.customer.billing_address.city }},
+                                {{ order.customer.billing_address.zipcode }} <br/>
+                                {{ order.customer.billing_address.state }},
+                                {{ order.customer.billing_address.country.name }} <br/>
+                            </div>
+                            <!--/  Billing Address Details-->
+                        </div>
+                        <div>
+                            <h2 class="text-xl font-semibold pb-2 border-b border-gray-600">
+                                Shipping Address
+                            </h2>
+                            <!--  Shipping Address Details-->
+                            <div>
+                                {{ order.customer.shipping_address.house_number }},
+                                {{ order.customer.shipping_address.area }} <br/>
+                                {{ order.customer.shipping_address.city }},
+                                {{ order.customer.shipping_address.zipcode }} <br/>
+                                {{ order.customer.shipping_address.state }},
+                                {{ order.customer.shipping_address.country.name }} <br/>
+                            </div>
+                            <!--/  Shipping Address Details-->
+                        </div>
+                    </div>
+                    <!--/  Customer Details-->
                 </div>
-                <div class="flex justify-between items-center">
-                  <div class="flex items-center">Qty: {{ item.quantity }}</div>
-                  <span class="text-lg font-semibold"> ${{ item.unit_price }} </span>
+                <!--/ Order Details -->
+
+                <!--    Order Items-->
+                <div>
+                    <h2 class="text-xl font-semibold mt-6 pb-2 border-b border-gray-600">
+                        Order Items
+                    </h2>
+                    <div v-for="item of order.items">
+                        <!-- Order Item -->
+                        <div class="flex flex-col sm:flex-row items-center gap-4 mt-6">
+                            <a
+                                :href="apiUrl + '/product/' + item.product.slug"
+                                class="w-36 h-32 flex items-center justify-center overflow-hidden"
+                            >
+                                <img :src="item.product.image" class="object-cover" :alt="item.product.title"/>
+                            </a>
+                            <div class="flex flex-col justify-between flex-1">
+                                <div class="flex justify-between mb-3">
+                                    <h3>
+                                        {{ item.product.title }}
+                                    </h3>
+                                </div>
+                                <div class="flex justify-between items-center">
+                                    <div class="flex items-center">Qty: {{ item.quantity }}</div>
+                                    <span class="text-lg font-semibold"> ${{ item.unit_price }} </span>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/ Order Item -->
+                        <hr v-if="item !== order.items[order.items.length - 1]" class="my-3 border-gray-600"/>
+                    </div>
                 </div>
-              </div>
+                <!--/    Order Items-->
+                <!--  Go to Orders -->
+                <router-link
+                    :to="{name: 'app.orders'}"
+                    class="mt-8 flex items-center gap-2 text-gray-600 dark:text-indigo-300 font-medium hover:text-indigo-600 hover:dark:text-indigo-400 transition-all"
+                >
+                    <ArrowLeftIcon class="size-6"/>
+                    <span>Back to Orders</span>
+                </router-link>
+                <!--/  Go to Orders -->
             </div>
-            <!--/ Order Item -->
-            <hr v-if="item !== order.items[order.items.length - 1]" class="my-3 border-gray-600" />
-          </div>
         </div>
-        <!--/    Order Items-->
-        <!--  Go to Orders -->
-        <router-link
-            :to="{name: 'app.orders'}"
-            class="mt-8 flex items-center gap-2 text-gray-600 dark:text-indigo-300 font-medium hover:text-indigo-600 hover:dark:text-indigo-400 transition-all"
-        >
-          <ArrowLeftIcon class="size-6" />
-          <span>Back to Orders</span>
-        </router-link>
-        <!--/  Go to Orders -->
-      </div>
-    </div>
-  </template>
+    </template>
 </template>
 
 <script setup>
@@ -172,36 +172,37 @@ const route = useRoute();
 const order = ref(null);
 const orderStatuses = ref([]);
 const loading = ref(true);
+const apiUrl = ref(import.meta.env.VITE_API_BASE_URL);
 
 onMounted(() => {
-  store.dispatch("getOrder", route.params.id).then(({data}) => {
-    order.value = data;
-    loading.value = false;
-  });
+    store.dispatch("getOrder", route.params.id).then(({data}) => {
+        order.value = data;
+        loading.value = false;
+    });
 
-  axiosClient.get("/orders/statuses").then(({data}) => {
-    orderStatuses.value = data.statuses;
-  });
+    axiosClient.get("/orders/statuses").then(({data}) => {
+        orderStatuses.value = data.statuses;
+    });
 });
 
 const changeStatus = (e) => {
-  const status = e.target.value;
-  axiosClient
-      .post(`/orders/change-status/${order.value.id}`, {status})
-      .then(({data}) => {
-        store.commit(
-            "showToast",
-            data.message || `Order status was successfully changed into "${status}"`
-        );
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    const status = e.target.value;
+    axiosClient
+        .post(`/orders/change-status/${order.value.id}`, {status})
+        .then(({data}) => {
+            store.commit(
+                "showToast",
+                data.message || `Order status was successfully changed into "${status}"`
+            );
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 
-  // axiosClient
-  //   .post(`/orders/change-status/${order.value.id}/${order.value.status}`)
-  //   .then(({ data }) => {
-  //     store.commit('showToast', `Order status was successfully changed into "${order.value.status}"`)
-  //   });
+    // axiosClient
+    //   .post(`/orders/change-status/${order.value.id}/${order.value.status}`)
+    //   .then(({ data }) => {
+    //     store.commit('showToast', `Order status was successfully changed into "${order.value.status}"`)
+    //   });
 };
 </script>
