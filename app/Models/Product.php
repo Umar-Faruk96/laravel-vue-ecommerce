f4aac00d6c0e32bfbb63925f83eea15078af7d44
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-14 20:50:11 +0600

    define image accessor to access image url on the site

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Sluggable\{HasSlug, SlugOptions};
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -37,5 +38,16 @@ class Product extends Model
     public function images(): HasMany
     {
         return $this->hasMany(ProductImage::class)->orderBy('position');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                $this->loadMissing('images');
+
+                return $this->images->first()?->url;
+            }
+        );
     }
 }
