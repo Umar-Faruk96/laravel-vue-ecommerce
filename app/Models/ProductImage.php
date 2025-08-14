66b51c898c11d9614c3ed22ca338d0c65b589eba
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-14 19:37:59 +0600

    define product relation with ProductImage model, declare fillables

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductImage extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'url',
+        'path',
+        'altText',
+        'mime',
+        'size',
+        'position'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
