ed2ba1560fcfe9b32c378f17b2236d1a8c8b7bde
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-14 16:41:46 +0600

    define product images table creation & import existing data from products table

--- database/migrations/2025_08_14_102641_create_product_images_table.php
+++ database/migrations/2025_08_14_102641_create_product_images_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +14,23 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Product::class, 'product_id')->constrained()->cascadeOnDelete();
+            $table->string('url', 255);
+            $table->string('path', 255)->nullable();
+            $table->text('altText')->nullable();
+            $table->string('mime', 50);
+            $table->integer('size');
+            $table->integer('position');
             $table->timestamps();
         });
+
+//        extract data of image, image_mime and image_size columns from products table
+        DB::table('product_images')->insert(
+            DB::table('products')
+                ->selectRaw('id, image, image_mime, image_size')
+                ->whereNotNull('image')
+                ->get()->toArray()
+        );
     }
 
     /**
