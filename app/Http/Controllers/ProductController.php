45e55493d2049afdb785b4edad1217c88ff478b7
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-15 22:39:33 +0600

    refactor codebase with correct return type

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         $products = Product::where('published', true)->orderByDesc('created_at')->paginate(10);
         return view('products.index', compact('products'));
     }
 
-    public function show(Product $product)
+    public function show(Product $product): View
     {
         return view('products.show', compact('product'));
     }
