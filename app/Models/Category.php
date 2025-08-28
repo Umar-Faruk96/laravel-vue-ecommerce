0ad1fad2d84a1e7a842422a75c6755f195fc4b33
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-28 23:57:01 +0600

    define model fillable, parent relation and slug options

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+    use HasSlug;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'active',
+        'parent_id',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()->generateSlugsFrom('name')->saveSlugsTo('slug');
+    }
+
+    public function parent() : BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
 }
