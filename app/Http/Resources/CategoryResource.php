c9615f8fdf468a5c7a9705750cf18ab62f29dbd9
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-29 23:21:55 +0600

    return resources on category

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -5,6 +5,16 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $id
+ * @property mixed $name
+ * @property mixed $slug
+ * @property mixed $active
+ * @property mixed $parent_id
+ * @property mixed $parent
+ * @property mixed $created_at
+ * @property mixed $updated_at
+ */
 class CategoryResource extends JsonResource
 {
     /**
@@ -14,6 +24,15 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'active' => $this->active,
+            'parent_id' => $this->parent_id,
+            'parent' => $this->parent ? new CategoryResource($this->parent) : null,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
