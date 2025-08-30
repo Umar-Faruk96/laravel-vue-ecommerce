d7533d580a9b3884df4d58e0877ae37880d80271
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-08-30 20:22:09 +0600

    add category component

---
+++ vue-admin-panel/src/views/Categories/Categories.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex items-center justify-between mb-3">
+    <h1 class="text-3xl font-semibold">Categories</h1>
+    <button type="button"
+            @click="showCategoryForm()"
+            class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+      Add new Category
+    </button>
+  </div>
+  <CategoriesTable @clickEdit="editCategory"/>
+  <CategoryModal v-model="categoryForm" :category="categoryData"
+                 @close="closeCategoryForm"/>
+</template>
+
+<script setup>
+import {computed, ref} from "vue";
+import store from "../../store";
+import CategoryModal from "./CategoryModal.vue";
+import CategoriesTable from "./CategoriesTable.vue";
+
+const DEFAULT_CATEGORY = {
+  id: '',
+  title: '',
+  description: '',
+  image: '',
+  price: ''
+}
+
+const categories = computed(() => store.state.categories);
+
+const categoryData = ref({...DEFAULT_CATEGORY})
+const categoryForm = ref(false);
+
+function showCategoryForm() {
+  categoryForm.value = true
+}
+
+function editCategory(u) {
+  categoryData.value = u;
+  showCategoryForm();
+}
+
+function closeCategoryForm() {
+  categoryData.value = {...DEFAULT_CATEGORY}
+}
+</script>
+
+<style scoped>
+
+</style>
