ea492f473f45723df50e764dc8e4b672ad07ec71
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2025-10-22 08:15:50 +0600

    define product and category relation on the product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -50,4 +50,9 @@ class Product extends Model
             }
         );
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'product_categories');
+    }
 }
